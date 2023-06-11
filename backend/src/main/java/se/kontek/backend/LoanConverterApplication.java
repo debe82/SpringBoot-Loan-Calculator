1364d1758b3d38ff0d692d3dd5708756df703eaf
marco debernardi <marco.debernardi.1982@gmail.com>
2023-06-11 10:41:30 +0200

    proj be created

---
+++ backend/src/main/java/se/kontek/backend/LoanConverterApplication.java
@@ -0,0 +1,13 @@
+package se.kontek.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanConverterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/se/kontek/backend/LoanConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kontek.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
